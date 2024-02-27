175660de382ebe2e68f388bc33eca381a1150392
Maxi Morales <39443928+mmo-uy@users.noreply.github.com>
2024-02-27 19:28:50 -0300

    feature: added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:osit_monitor/controllers/app_controller.dart';
-import 'package:osit_monitor/controllers/main_wrapper_controller.dart';
-import 'package:osit_monitor/controllers/nfc_controller.dart';
-import 'package:osit_monitor/controllers/qr_controller.dart';
+import 'package:osit_monitor/firebase_options.dart';
 import 'package:osit_monitor/helpers/utils.dart';
 import 'package:osit_monitor/screens/home_page.dart';
-import 'package:osit_monitor/services/app_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await AppUtils.initServices();
   runApp(const OSitMonitorApp());
 }
